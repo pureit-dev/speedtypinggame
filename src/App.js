fefaaf29799d25ad715af7149c5a412a55b6e729
Keith Gibson <keithwgibson74@gmail.com>
2023-01-27 10:34:01 +0000

    Setup initial structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./App.css"
+
+const App = () => {
+	return (
+  
+    <div>
+      <h1>How fast can you type?</h1>
+      <textarea />
+      <h4>Time remaining: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
+    </div>
+  )
+}
+
+export default App
