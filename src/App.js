6c6d265eed62eef3e011ab0c33e9c15ed4115e53
Keith Gibson <keithwgibson74@gmail.com>
2023-01-27 11:07:45 +0000

    Added state and functions to update state onChange and onClick

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from "react"
+import {useState} from "react"
 import "./App.css"
 
 const App = () => {
+
+   const [ text, setText ] = useState("")
+
+    const handleChange = (e) => (
+      setText(e.target.value)
+    )
+      
+    const calculateWordCount = (text) => {
+      return (text
+			.trim()
+			.split(" ")
+			.filter((word) => word !== "").length)
+    }
+    
+    console.log(text)
 	return (
   
     <div>
       <h1>How fast can you type?</h1>
-      <textarea />
+      <textarea onChange={handleChange} value={text}/>
       <h4>Time remaining: ???</h4>
-      <button>Start</button>
+      <button onClick={() => calculateWordCount(text)} >Start</button>
       <h1>Word count: ???</h1>
     </div>
   )
