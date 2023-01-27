d3332094e308a9907c7abc9d27efe7c86397fa53
Keith Gibson <keithwgibson74@gmail.com>
2023-01-27 11:27:34 +0000

    Added useEffect to countdown timer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import "./App.css"
 
 const App = () => {
 
    const [ text, setText ] = useState("")
+   const [timeRemaining, setTimeRemaining] = useState(5)
 
     const handleChange = (e) => (
       setText(e.target.value)
@@ -16,17 +17,27 @@ const App = () => {
 			.filter((word) => word !== "").length)
     }
     
-    console.log(text)
+    useEffect(() => {
+      if (timeRemaining > 0) {
+			setTimeout(() => {
+				setTimeRemaining(
+					(prevTime) => (prevTime - 1)
+					,
+					[timeRemaining]
+				)
+			}, 1000)
+		}
+    })
+   
 	return (
-  
-    <div>
-      <h1>How fast can you type?</h1>
-      <textarea onChange={handleChange} value={text}/>
-      <h4>Time remaining: ???</h4>
-      <button onClick={() => calculateWordCount(text)} >Start</button>
-      <h1>Word count: ???</h1>
-    </div>
-  )
+		<div>
+			<h1>How fast can you type?</h1>
+			<textarea onChange={handleChange} value={text} />
+			<h4>Time remaining: {timeRemaining}</h4>
+			<button onClick={() => calculateWordCount(text)}>Start</button>
+			<h1>Word count: ???</h1>
+		</div>
+	)
 }
 
 export default App
